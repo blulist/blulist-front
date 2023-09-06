f4879c5728b5c72988500a8631881ae53923bd8a
sajjad isvand <sajjadzibaee102@gmail.com>
2023-09-06 14:23:21 +0330

    update texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import NavBar from "@/components/NavBar";
 import Link from "next/link";
 import { BiLogoTelegram } from "react-icons/bi";
+import {MdExplore} from "react-icons/md";
 export default function Home() {
     return (
         <section className="h-screen w-full main-gradient flex flex-col">
@@ -12,20 +13,27 @@ export default function Home() {
             >
                 <div className="flex flex-col gap-4 justify-center items-center px-2 text-center md:text-right md:gap-10 md:items-start max-w-[700px]">
                     <h1 className="text-5xl text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-blue-700 leading-[1.4] md:text-right md:text-6xl md:leading-[1.4] max-w-[550px]">
-                        ما تو تلگرام پلی لیست جمع میکنیم آقا
+                       Bluلـیسـت
                     </h1>
                     <p className="text-white/50 max-w-[500px] md:text-lg">
-                        اینجا یچزی مینویسم که مثلا ما کارمون درسته.گول بخورید
-                        گول بخورید گول بخورید گول بخورید گول بخورید گول بخورید
-                        گول بخورید
+                        بـا بلـولیست  پلی لیسـت خودتون رو گوش بدید،به اشتراک بگذارید، بین پلی لیسـت های مختلف کاووش کنید!
                     </p>
-                    <Link
-                        href={"https://t.me/"}
-                        className="bg-blue-600/30 border border-blue-500/50 rounded-xl flex gap-2 px-6 py-3 transition-all duration-300 hover:bg-blue-600"
-                    >
-                        <BiLogoTelegram />
-                        شروع کار با ربات
-                    </Link>
+                    <div className={"flex flex-row gap-2"}>
+                        <Link
+                            href={"https://t.me/"}
+                            className="bg-blue-600/30 border border-blue-500/50 rounded-xl flex gap-2 px-6 py-3 transition-all duration-300 hover:bg-blue-600"
+                        >
+                            <BiLogoTelegram />
+                            شروع کار با ربات
+                        </Link>
+                        <Link
+                            href={"/playlists"}
+                            className="bg-blue-600/30 border border-blue-500/50 rounded-xl flex gap-2 px-6 py-3 transition-all duration-300 hover:bg-blue-600"
+                        >
+                            <MdExplore />
+                            کاووش در پلی لیست ها
+                        </Link>
+                    </div>
                 </div>
                 <div>
                     <Image
